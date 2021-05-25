cccefdf0e088570655b828e04497b27411fb6a54
alicia1115 <83991079+alicia1115@users.noreply.github.com>
2021-05-25 09:55:34 +0900

    Add files via upload

--- 보드게임.c
+++ 보드게임.c
@@ -21,13 +21,19 @@ void main(void)
 	int position = 0;
 	//주사위
 	int dice;
+	//돈은 2억부터 시작(좀 더 많은 숫자를 담기 위하여 long long으로 처리)
+	long long money = 200000000;
 
 	srand(time(0));
-	printf("현재위치 : %d", position);
+
+	printf("현재위치 : %d, 현재금액 : %lld \n\n", position, money);
 
 	//무한반복
 	while (1)
 	{
+		//엔터키를 눌러서 진행시켜
+		getchar();
+
 		/* 던짐 */
 	   //주사위는 1~6까지 나오게 설정
 		dice = rand() % 6 + 1;
@@ -35,18 +41,8 @@ void main(void)
 
 		//던진 주사위 값을 현재 위치에 적용
 		position += dice;
-		printf("현재위치 : %d\n\n", position);
-		getchar();
+		printf("현재위치 : %d, 현재금액 : %lld \n\n", position, money);
 
-		/*두 번째 던짐*/
-		//주사위는 1~6까지 나오게 설정
-		dice = rand() % 6 + 1;
-		printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
-
-		//던진 주사위 값을 현재 위치에 적용
-		position += dice;
-		printf("현재위치 : %d\n\n", position);
-		
 		if (position > 60)
 		{
 			printf("축하합니다. 화성에 도착하셨습니다");
