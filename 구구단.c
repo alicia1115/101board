133fbefd1f6a92626cb0b1b09ffad6ecf5e729b0
alicia1115 <83991079+alicia1115@users.noreply.github.com>
2021-06-15 10:03:30 +0900

    [문법연습] 한 줄짜리 코드의 경우, 중괄호 제거

--- 구구단.c
+++ 구구단.c
@@ -4,15 +4,11 @@ void main(void)
 	for (int dan = 2; dan <= 9; dan++)
 	{
 		if (dan == 4)
-		{
 			continue; //바깥쪽 for문에 대한 break(4단만 skip)
-		}
 		for (int i = 1; i <= 9; i++)
 		{
 			if (i == 5)
-			{
 				continue; //안쪽 for문에 대한 continue(수5만 skip)
-			}
 			printf("%d x %d = %d \n", dan, i, dan * i);
 		}
 		printf("\n");
