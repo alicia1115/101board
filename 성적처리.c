59dc673cbf86448e4f1ff7fd73918f1bf7088f55
chojangg <83991079+chojangg@users.noreply.github.com>
2021-08-31 09:35:31 +0900

    함수화

--- 성적처리.c
+++ 성적처리.c
@@ -1,54 +1,41 @@
 #include <stdio.h>
 
-void main(void)
+// 입력받은 점수에 대한 등급을 출력
+void cal_grade(int score)
 {
-	int c_score;
-	printf("C언어 점수를 입력하시오 : ");
-	scanf_s("%d", &c_score);
-
-	if (c_score >= 90 && c_score <= 100)
+	if (score >= 90 && score <= 100)
 		printf("A");
-	else if (c_score >= 80)
+	else if (score >= 80)
 		printf("B");
-	else if (c_score >= 70)
+	else if (score >= 70)
 		printf("C");
-	else if (c_score >= 60)
+	else if (score >= 60)
 		printf("D");
 	else
 		printf("E");
+
 	printf("\n");
+}
+
+void main(void)
+{
+	int C_score;
+	printf("C언어 점수를 입력하시오 : ");
+	scanf("%d", &C_score);
+
+	cal_grade(C_score);
 
 	int java_score;
 	printf("자바 점수를 입력하시오 : ");
-	scanf_s("%d", &java_score);
+	scanf("%d", &java_score);
 
-	if (java_score >= 90 && java_score <= 100)
-		printf("A");
-	else if (java_score >= 80)
-		printf("B");
-	else if (java_score >= 70)
-		printf("C");
-	else if (java_score >= 60)
-		printf("D");
-	else
-		printf("E");
-	printf("\n");
+	cal_grade(java_score);
 
-	int ca_score;
+	int CA_score;
 	printf("컴퓨터구조 점수를 입력하시오 : ");
-	scanf_s("%d", &ca_score);
+	scanf("%d", &CA_score);
 
-	if (ca_score >= 90 && ca_score <= 100)
-		printf("A");
-	else if (ca_score >= 80)
-		printf("B");
-	else if (ca_score >= 70)
-		printf("C");
-	else if (ca_score >= 60)
-		printf("D");
-	else
-		printf("E");
-	printf("\n");
+	cal_grade(CA_score);
 
 	return 0;
 }
