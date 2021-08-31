b8ed19a3e85930781b565ed13f1004981bbe016c
chojangg <83991079+chojangg@users.noreply.github.com>
2021-08-31 09:44:04 +0900

    함수의 정방향선언

--- 성적처리.c
+++ 성적처리.c
@@ -1,41 +1,34 @@
-#include <stdio.h>
+#include<stdio.h>
+
+void cal_grade(int score);
+
+void main(void)
+{
+	int C_score, java_score, CA_score;
+	printf("씨언어 점수를 입력하시오 : ");
+	scanf_s("%d", &C_score);
+	cal_grade(C_score);
+
+	printf("자바 점수를 입력하시오 : ");
+	scanf_s("%d", &java_score);
+	cal_grade(java_score);
+
+	printf("컴퓨터구조 점수를 입력하시오 : ");
+	scanf_s("%d", &CA_score);
+	cal_grade(CA_score);
+}
 
 // 입력받은 점수에 대한 등급을 출력
 void cal_grade(int score)
 {
 	if (score >= 90 && score <= 100)
-		printf("A");
+		printf("A\n");
 	else if (score >= 80)
-		printf("B");
+		printf("B\n");
 	else if (score >= 70)
-		printf("C");
+		printf("C\n");
 	else if (score >= 60)
-		printf("D");
+		printf("D\n");
 	else
-		printf("E");
-
-	printf("\n");
-}
-
-void main(void)
-{
-	int C_score;
-	printf("C언어 점수를 입력하시오 : ");
-	scanf("%d", &C_score);
-
-	cal_grade(C_score);
-
-	int java_score;
-	printf("자바 점수를 입력하시오 : ");
-	scanf("%d", &java_score);
-
-	cal_grade(java_score);
-
-	int CA_score;
-	printf("컴퓨터구조 점수를 입력하시오 : ");
-	scanf("%d", &CA_score);
-
-	cal_grade(CA_score);
-
-	return 0;
+		printf("E\n");
 }
