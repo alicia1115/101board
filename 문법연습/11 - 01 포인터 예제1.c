e3a3021ed74e0460beab7bc37278ed4711532677
chojangg <83991079+chojangg@users.noreply.github.com>
2021-09-13 13:54:31 +0900

    포인터 예제 01

---
+++ 문법연습/11 - 01 포인터 예제1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int num = 3;
+	int* ptr = &num;
+	//int *ptr = &num; 두가지 방법
+
+	printf("num안에 들어있는 값 %d\n", num);
+	printf("num의 메모리 주소 값 %d\n", &num);
+	printf("ptr안에 들어있는 값 %d\n", ptr);
+}
