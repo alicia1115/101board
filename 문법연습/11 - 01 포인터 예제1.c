97ba237b3ba392fa455dd78b57c298616e9da8e1
chojangg <83991079+chojangg@users.noreply.github.com>
2021-09-13 13:56:16 +0900

    주소값을 16진수로

--- 문법연습/11 - 01 포인터 예제1.c
+++ 문법연습/11 - 01 포인터 예제1.c
@@ -7,6 +7,6 @@ void main(void)
 	//int *ptr = &num; 두가지 방법
 
 	printf("num안에 들어있는 값 %d\n", num);
-	printf("num의 메모리 주소 값 %d\n", &num);
-	printf("ptr안에 들어있는 값 %d\n", ptr);
+	printf("num의 메모리 주소 값 %p\n", &num);
+	printf("ptr안에 들어있는 값 %p\n", ptr);
 }
